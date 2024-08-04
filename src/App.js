a2bc1c536a81b341ff69a452bbada09c707bd1a8
shivam0870 <shivamkrsingh4688@gmail.com>
2024-08-04 23:12:20 +0530

    added xdisplayname

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import Xcountries from "./Components/XCountries";
 import XStopWatch from "./Components/XStopwatch";
 import Sample from "./Components/Sample";
+import XDisplayName from "./Components/XDisplayName";
 
 const App = () => {
   return (
     <>
       {/* <Xcountries/> */}
-      <XStopWatch/>
-      
+      {/* <XStopWatch/> */}
+      <XDisplayName/>
     </>
   );
 }
