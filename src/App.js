ba0bac3e11a381f56a800413a7afaea3ee0ee209
shivam0870 <shivamkrsingh4688@gmail.com>
2024-08-07 15:04:21 +0530

    added xcounter

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Xcountries from "./Components/XCountries";
 import XStopWatch from "./Components/XStopwatch";
 import Sample from "./Components/Sample";
 import XDisplayName from "./Components/XDisplayName";
+import XCounter from "./Components/XCounter";
 
 const App = () => {
   return (
     <>
       {/* <Xcountries/> */}
       {/* <XStopWatch/> */}
-      <XDisplayName/>
+      {/* <XDisplayName/> */}
+      <XCounter/>
     </>
   );
 }
